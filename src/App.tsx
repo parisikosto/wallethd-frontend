c9bafda0daffdd030a9de4b24f102d107867aaaa
Paris Kostopoulos <parisikosto@gmail.com>
2025-11-13 22:37:23 +0200

    feat(*): add dummy routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
-export const App = (): React.JSX.Element => {
-  return <>App</>;
+import type { JSX } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import { HomePage, LoginPage, SettingsPage, TransactionsPage } from './pages';
+
+export const App = (): JSX.Element => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="transactions" element={<TransactionsPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
