5262c26ba6d4590a23e7856480d0605d781b6be4
Paris Kostopoulos <parisikosto@gmail.com>
2025-11-13 20:58:42 +0200

    feat(components): add Skeleton ui component

---
+++ src/components/ui/Skeleton/Skeleton.tsx
@@ -0,0 +1,16 @@
+import type { ComponentProps, JSX } from 'react';
+
+import { cn } from '@/lib/utils';
+
+export const Skeleton = ({
+  className,
+  ...props
+}: ComponentProps<'div'>): JSX.Element => {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn('bg-accent animate-pulse rounded-md', className)}
+      {...props}
+    />
+  );
+};
