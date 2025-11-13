04b20bea4fe5bb470eab5b66b3706818e5366eb7
Paris Kostopoulos <parisikosto@gmail.com>
2025-11-13 19:29:08 +0200

    feat(components): add Button ui component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,28 @@
+import type { ComponentProps, JSX } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+import { buttonVariants } from './buttonVariants';
+
+export const Button = ({
+  asChild = false,
+  className,
+  size,
+  variant,
+  ...props
+}: ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }): JSX.Element => {
+  const Comp = asChild ? Slot : 'button';
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+};
